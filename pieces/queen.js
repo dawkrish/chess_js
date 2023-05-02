d4e96c4d879e46d06bf75f90fc005c6d2d4dd92b
krishnanshagarwal112 <gonkillua2680@gmail.com>
2023-05-02 15:35:16 +0530

    queen

--- pieces/queen.js
+++ pieces/queen.js
@@ -1,5 +1,5 @@
 import { Piece } from "../pieces.js"
-
+import {dict} from "../vars.js"
 export class Queen extends Piece{
     constructor(board,piece_color,piece_position){
        super(board,piece_color,piece_position)
@@ -10,5 +10,339 @@ export class Queen extends Piece{
         this.img = 'images/wq.png'
        }
     }
-}
+    valid_moves(){
+        let valid_moves = []
+        let pos = dict[this.piece_position]
 
+        if(this.piece_color == "white"){
+            if(this.board.chance == "black"){
+                return []
+            }
+            // Rook Movements
+            // UP
+            let i = pos[0]
+            let j = pos[1]
+
+            while(i-1 >= 0){
+                let target_pos = this.board.arr[i-1][j]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([i-1,j])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "black"){
+                    valid_moves.push([i-1,j])
+                }
+                // break if the position is not empty
+                if(target_pos.piece != null) break
+                i--
+            }
+
+            // DOWN
+            i = pos[0]
+            j = pos[1]
+
+            while(i+1 <= 7){
+                let target_pos = this.board.arr[i+1][j]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([i+1,j])
+                    
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "black"){
+                    valid_moves.push([i+1,j])
+                }
+                // break if the position is not empty
+                if(target_pos.piece != null) break
+                i++
+            }
+
+            // LEFT
+            i = pos[0]
+            j = pos[1]
+
+            while(j-1 >= 0){
+                let target_pos = this.board.arr[i][j-1]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([i,j-1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "black"){
+                    valid_moves.push([i,j-1])
+                }
+                // break if the position is not empty
+                if(target_pos.piece != null) break
+                j--
+            }
+
+            // RIGHT
+            i = pos[0]
+            j = pos[1]
+
+            while(j+1 <= 7){
+                let target_pos = this.board.arr[i][j+1]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([i,j+1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "black"){
+                    valid_moves.push([i,j+1])
+                }
+                // break if the position is not empty
+                if(target_pos.piece != null) break
+                j++
+            }
+
+            // Bishop movements !
+            // UP LEFT DIAGONAL
+             i = pos[0]
+             j = pos[1]
+
+            while(i-1 >= 0 && j - 1 >= 0){
+                let target_pos = this.board.arr[i-1][j-1]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([i-1,j-1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "black"){
+                    valid_moves.push([i-1,j-1])
+                }
+                // break if the position is not empty
+                if(target_pos.piece != null) break
+                i--
+                j--
+            }
+
+            // UP RIGHT
+            i = pos[0]
+            j = pos[1]
+
+            while(i-1 >= 0 && j+1 <= 7){
+                let target_pos = this.board.arr[i-1][j+1]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([i-1,j+1])
+                    
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "black"){
+                    valid_moves.push([i-1,j+1])
+                }
+                // break if the position is not empty
+                if(target_pos.piece != null) break
+                i--
+                j++
+            }
+
+            // DOWN LEFT
+            i = pos[0]
+            j = pos[1]
+
+            while(j-1 >= 0 && i+1 <= 7){
+                let target_pos = this.board.arr[i+1][j-1]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([i+1,j-1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "black"){
+                    valid_moves.push([i+1,j-1])
+                }
+                // break if the position is not empty
+                if(target_pos.piece != null) break
+                j--
+                i++
+            }
+
+            // DOWN RIGHT
+            i = pos[0]
+            j = pos[1]
+
+            while(j+1 <= 7 && i + 1 <= 7){
+                let target_pos = this.board.arr[i+1][j+1]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([i+1,j+1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "black"){
+                    valid_moves.push([i+1,j+1])
+                }
+                // break if the position is not empty
+                if(target_pos.piece != null) break
+                j++
+                i++
+            }
+        }
+        else{
+            if(this.board.chance == "white"){
+                return []
+            }
+            // ROOK MOVEMENTS !
+            // UP
+            let i = pos[0]
+            let j = pos[1]
+
+            while(i-1 >= 0){
+                let target_pos = this.board.arr[i-1][j]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([i-1,j])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "white"){
+                    valid_moves.push([i-1,j])
+                }
+                // break if the position is not empty
+                if(target_pos.piece != null) break
+                i--
+            }
+
+            // DOWN
+            i = pos[0]
+            j = pos[1]
+
+            while(i+1 <= 7){
+                let target_pos = this.board.arr[i+1][j]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([i+1,j])
+                    
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "white"){
+                    valid_moves.push([i+1,j])
+                }
+                // break if the position is not empty
+                if(target_pos.piece != null) break
+                i++
+            }
+
+            // LEFT
+            i = pos[0]
+            j = pos[1]
+
+            while(j-1 >= 0){
+                let target_pos = this.board.arr[i][j-1]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([i,j-1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "white"){
+                    valid_moves.push([i,j-1])
+                }
+                // break if the position is not empty
+                if(target_pos.piece != null) break
+                j--
+            }
+
+            // RIGHT
+            i = pos[0]
+            j = pos[1]
+
+            while(j+1 <= 7){
+                let target_pos = this.board.arr[i][j+1]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([i,j+1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "white"){
+                    valid_moves.push([i,j+1])
+                }
+                // break if the position is not empty
+                if(target_pos.piece != null) break
+                j++
+            }
+
+            // Bishop movements
+            // UP LEFT DIAGONAL
+            i = pos[0]
+            j = pos[1]
+
+            while(i-1 >= 0 && j - 1 >= 0){
+                let target_pos = this.board.arr[i-1][j-1]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([i-1,j-1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "white"){
+                    valid_moves.push([i-1,j-1])
+                }
+                // break if the position is not empty
+                if(target_pos.piece != null) break
+                i--
+                j--
+            }
+
+            // UP RIGHT
+            i = pos[0]
+            j = pos[1]
+
+            while(i-1 >= 0 && j+1 <= 7){
+                let target_pos = this.board.arr[i-1][j+1]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([i-1,j+1])
+                    
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "white"){
+                    valid_moves.push([i-1,j+1])
+                }
+                // break if the position is not empty
+                if(target_pos.piece != null) break
+                i--
+                j++
+            }
+
+            // DOWN LEFT
+            i = pos[0]
+            j = pos[1]
+
+            while(j-1 >= 0 && i+1 <= 7){
+                let target_pos = this.board.arr[i+1][j-1]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([i+1,j-1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "white"){
+                    valid_moves.push([i+1,j-1])
+                }
+                // break if the position is not empty
+                if(target_pos.piece != null) break
+                j--
+                i++
+            }
+
+            // DOWN RIGHT
+            i = pos[0]
+            j = pos[1]
+
+            while(j+1 <= 7 && i + 1 <= 7){
+                let target_pos = this.board.arr[i+1][j+1]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([i+1,j+1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "white"){
+                    valid_moves.push([i+1,j+1])
+                }
+                // break if the position is not empty
+                if(target_pos.piece != null) break
+                j++
+                i++
+            }
+        }
+        
+        return valid_moves
+    }
+}
