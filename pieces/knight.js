c76edb02b00a025ed6ce6f7cc8e854fc7c8fdaba
krishnanshagarwal112 <gonkillua2680@gmail.com>
2023-05-02 13:23:50 +0530

    knight valid moves

--- pieces/knight.js
+++ pieces/knight.js
@@ -1,4 +1,5 @@
 import { Piece } from "../pieces.js"
+import { dict } from "../vars.js"
 
 export class Knight extends Piece{
     constructor(board,piece_color,piece_position){
@@ -9,6 +10,232 @@ export class Knight extends Piece{
        else{
         this.img = 'images/wn.png'
        }
+    }
+
+    valid_moves(){
+        let valid_moves = []
+
+        let pos = dict[this.piece_position]
+        // First for white
+        if(this.piece_color == "white"){
+        
+            // left up up square
+            if(pos[0]-2 >=0 && pos[1]-1 >= 0){
+                let target_pos = this.board.arr[pos[0]-2][pos[1]-1]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([pos[0]-2,pos[1]-1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "black"){
+                    valid_moves.push([pos[0]-2,pos[1]-1])
+                }
+            }
+
+            // left left up square
+            if(pos[0]-1 >=0 && pos[1]-2 >= 0){
+                let target_pos = this.board.arr[pos[0]-1][pos[1]-2]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([pos[0]-1,pos[1]-2])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "black"){
+                    valid_moves.push([pos[0]-1,pos[1]-2])
+                }
+            }
+
+            // right up up square
+            if(pos[0]-2 >=0 && pos[1]+1 <= 7){
+                let target_pos = this.board.arr[pos[0]-2][pos[1]+1]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([pos[0]-2,pos[1]+1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "black"){
+                    valid_moves.push([pos[0]-2,pos[1]+1])
+                }
+            }
+
+            // right right up square
+            if(pos[0]-1 >=0 && pos[1]+2 <= 7){
+                let target_pos = this.board.arr[pos[0]-1][pos[1]+2]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([pos[0]-1,pos[1]+2])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "black"){
+                    valid_moves.push([pos[0]-1,pos[1]+2])
+                }
+            }
+
+
+
+            // left down down square
+            if(pos[0]+2 <=7 && pos[1]-1 >= 0){
+                let target_pos = this.board.arr[pos[0]+2][pos[1]-1]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([pos[0]+2,pos[1]-1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "black"){
+                    valid_moves.push([pos[0]+2,pos[1]-1])
+                }
+            }
+
+            // left left down square
+            if(pos[0]+1 <=7 && pos[1]-2 >= 0){
+                let target_pos = this.board.arr[pos[0]+1][pos[1]-2]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([pos[0]+1,pos[1]-2])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "black"){
+                    valid_moves.push([pos[0]+1,pos[1]-2])
+                }
+            }
+
+            // right down down square
+            if(pos[0]+2 <=7 && pos[1]+1 <= 7){
+                let target_pos = this.board.arr[pos[0]+2][pos[1]+1]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([pos[0]+2,pos[1]+1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "black"){
+                    valid_moves.push([pos[0]+2,pos[1]+1])
+                }
+            }
+
+            // right right down square
+            if(pos[0]+1 <=7 && pos[1]+2 <= 7){
+                let target_pos = this.board.arr[pos[0]+1][pos[1]+2]
+                // Condition -> at target_pos there must be not a white piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([pos[0]+1,pos[1]+2])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "black"){
+                    valid_moves.push([pos[0]+1,pos[1]+2])
+                }
+            }
+
+
+           
+
+        }
+       
+        else{
+            // left up up square
+            if(pos[0]+2 <=7 && pos[1]-1 >= 0){
+                let target_pos = this.board.arr[pos[0]+2][pos[1]-1]
+                // Condition -> at target_pos there must be not a black piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([pos[0]+2,pos[1]-1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "white"){
+                    valid_moves.push([pos[0]+2,pos[1]-1])
+                }
+            }
+            // left left up square
+            if(pos[0]+1 <=7 && pos[1]-2 >= 0){
+                let target_pos = this.board.arr[pos[0]+1][pos[1]-2]
+                // Condition -> at target_pos there must be not a black piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([pos[0]+1,pos[1]-2])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "white"){
+                    valid_moves.push([pos[0]+1,pos[1]-2])
+                }
+            }
+
+            // right up up square
+            if(pos[0]+2 <=7 && pos[1]+1 <= 7){
+                let target_pos = this.board.arr[pos[0]+2][pos[1]+1]
+                // Condition -> at target_pos there must be not a black piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([pos[0]+2,pos[1]+1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "white"){
+                    valid_moves.push([pos[0]+2,pos[1]+1])
+                }
+            }
+            // right right up square
+            if(pos[0]+1 <=7 && pos[1]+2 <= 7){
+                let target_pos = this.board.arr[pos[0]+1][pos[1]+2]
+                
+                // Condition -> at target_pos there must be not a black piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([pos[0]+1,pos[1]+2])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "white"){
+                    valid_moves.push([pos[0]+1,pos[1]+2])
+                }
+            }
+
+            // left down down 
+            if(pos[0]-2 >=0 && pos[1] -1 >= 0){
+                let target_pos = this.board.arr[pos[0]-2][pos[1]-1]
+                 // Condition -> at target_pos there must be not a black piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([pos[0]-2,pos[1]-1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "white"){
+                    valid_moves.push([pos[0]-2,pos[1]-1])
+                }
+            }
+
+            // left left down
+            if(pos[0]-1 >=0 && pos[1] -2 >= 0){
+                let target_pos = this.board.arr[pos[0]-1][pos[1]-2]
+                 // Condition -> at target_pos there must be not a black piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([pos[0]-1,pos[1]-2])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "white"){
+                    valid_moves.push([pos[0]-1,pos[1]-2])
+                }
+            }
+
+            // right down down 
+            if(pos[0]-2 >=0 && pos[1] + 1 <= 7){
+                let target_pos = this.board.arr[pos[0]-2][pos[1]+1]
+                 // Condition -> at target_pos there must be not a black piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([pos[0]-2,pos[1]+1])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "white"){
+                    valid_moves.push([pos[0]-2,pos[1]+1])
+                }
+            }
+
+            // right right down
+            if(pos[0]-1 >=0 && pos[1] +2 <= 7){
+                let target_pos = this.board.arr[pos[0]-1][pos[1]+2]
+                 // Condition -> at target_pos there must be not a black piece
+                // first check whether there is a piece at the board or not
+                if(target_pos.piece == null){
+                    valid_moves.push([pos[0]-1,pos[1]+2])
+                }
+                if(target_pos.piece != null && target_pos.piece.piece_color == "white"){
+                    valid_moves.push([pos[0]-1,pos[1]+2])
+                }
+            }
+        }
+
+        return valid_moves
     }
 }
 
