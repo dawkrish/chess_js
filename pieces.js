7fd1cc301b1f5d3f440f406479a631d591d239ce
krishnanshagarwal112 <gonkillua2680@gmail.com>
2023-05-05 17:46:55 +0530

    pieces class

--- pieces.js
+++ pieces.js
@@ -1,21 +1,18 @@
-import { Position } from "./position.js"
-import { dict } from "./vars.js"
+import { Position } from "./position.js";
+import { dict } from "./vars.js";
 export class Piece {
-    constructor(board,piece_color, piece_position){
-        this.board=board
-        this.piece_color=piece_color
-        this.piece_position = piece_position
-    }
-    move(to){
-        console.log('running the move function ')
-        let from_p = dict[this.piece_position]
-
-
-        this.board.arr[to[0]][to[1]].piece = this.board.arr[from_p[0]][from_p[1]].piece
-        this.board.arr[from_p[0]][from_p[1]].piece = null
-        this.board.arr[to[0]][to[1]].piece.piece_position = `${String.fromCharCode(65 + Number(to[1]))}${8 - Number(to[0])}`
-
-    }
+  constructor(board, piece_color, piece_position) {
+    this.board = board;
+    this.piece_color = piece_color;
+    this.piece_position = piece_position;
+  }
+  move(to) {
+    let from_p = dict[this.piece_position];
+    this.board.arr[to[0]][to[1]].piece =
+      this.board.arr[from_p[0]][from_p[1]].piece;
+    this.board.arr[from_p[0]][from_p[1]].piece = null;
+    this.board.arr[to[0]][to[1]].piece.piece_position = `${String.fromCharCode(
+      65 + Number(to[1])
+    )}${8 - Number(to[0])}`;
+  }
 }
-
-
