31175c9e9f0b05701bb0efca784b043e7e4c9df8
krishnanshagarwal112 <gonkillua2680@gmail.com>
2023-05-05 17:43:52 +0530

    bishop done

--- pieces/bishop.js
+++ pieces/bishop.js
@@ -215,7 +215,6 @@ export class Bishop extends Piece {
       }
     }
     if (my_king.is_in_check() == true) {
-      console.log(this.board);
       let new_valid_moves = [];
       valid_moves.forEach((pos) => {
         // I will place the piece on the valid pos, and check if the king is still in check
@@ -229,23 +228,23 @@ export class Bishop extends Piece {
         this.board.arr[pos[0]][pos[1]].piece.move(my_pos); // moving the self piece back to my_pos
         this.board.arr[pos[0]][pos[1]].piece = piece_at_valid_position; // placing the piece which was replaced back to its original position !
       });
-      console.log(this.board);
+
       return new_valid_moves;
     }
-    let i = 0
-    while(i < valid_moves.length){
+    let i = 0;
+    while (i < valid_moves.length) {
       let my_pos = dict[this.piece_position]; // get self piece position
-      let new_pos = valid_moves[i]
+      let new_pos = valid_moves[i];
       let piece_at_new_position = this.board.arr[new_pos[0]][new_pos[1]].piece; // get the piece at the valid pos , we will need to put it back !
       this.board.arr[my_pos[0]][my_pos[1]].piece.move(valid_moves[i]); // moving self piece to the valid pos
       if (my_king.is_in_check() == true) {
         valid_moves.splice(i, 1);
-        i-- // need to do because valid move array's length is reduced therefore i also need to be removed, try with a example 
+        i--; // need to do because valid move array's length is reduced therefore i also need to be removed, try with a example
         // let valid move arr have 2 elements, suppose 1st is removed, then valid move array length becomes 1, so for next itereation 1 < 1; to avoid this we decrease i -> i- 1 ; so when it is incremented below , it turns i->0 .
       }
       this.board.arr[new_pos[0]][new_pos[1]].piece.move(my_pos); // moving the self piece back to my_pos
       this.board.arr[new_pos[0]][new_pos[1]].piece = piece_at_new_position; // placing the piece which was replaced back to its original position !
-      i++
+      i++;
     }
     return valid_moves;
   }
