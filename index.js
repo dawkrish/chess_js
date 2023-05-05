37607299614b550677e81d9cc395f5f8e1f46303
krishnanshagarwal112 <gonkillua2680@gmail.com>
2023-05-05 17:47:58 +0530

    entry point

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-import { Board } from "./board.js"
-import {dict} from "./vars.js"
+import { Board } from "./board.js";
+import { dict } from "./vars.js";
 
+export const root = document.getElementById("root");
 
-export const root = document.getElementById('root')
+export const board = new Board();
+console.log(dict);
+root.appendChild(board.render());
 
-export const board = new Board()
-console.log(board)
+let grid = document.getElementsByClassName("square");
+export let squares = convertToBoard(grid);
 
-root.appendChild(board.render())
-
-
-let grid = document.getElementsByClassName('square');
-export let squares = convertToBoard(grid)
-
-function convertToBoard(arr){
-    let i =0
-    let j = 0
-    let rows = []
-    while(i < 64){
-        let r = []
-        while(j < 8){
-            r.push(arr[i])
-            i++;
-            j++;
-        }
-        j = 0
-        rows.push(r)
+function convertToBoard(arr) {
+  let i = 0;
+  let j = 0;
+  let rows = [];
+  while (i < 64) {
+    let r = [];
+    while (j < 8) {
+      r.push(arr[i]);
+      i++;
+      j++;
     }
-    return rows
+    j = 0;
+    rows.push(r);
+  }
+  return rows;
 }
-
-
