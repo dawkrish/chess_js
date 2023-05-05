b55030eea4ce622c4739eb5cc73bbde2bf126e23
krishnanshagarwal112 <gonkillua2680@gmail.com>
2023-05-05 17:47:45 +0530

    dict mapping chess coordinates to array coordinates

--- vars.js
+++ vars.js
@@ -1,9 +1,7 @@
-export let dict = {
-    
-}
-for(let i = 0; i < 8; i++){
-        for(let j = 0; j < 8; j++){
-            let coordinate = `${String.fromCharCode(65+j)}${8-i}`
-            dict[coordinate] = [i,j]
-        }
+export let dict = {};
+for (let i = 0; i < 8; i++) {
+  for (let j = 0; j < 8; j++) {
+    let coordinate = `${String.fromCharCode(65 + j)}${8 - i}`;
+    dict[coordinate] = [i, j];
+  }
 }
